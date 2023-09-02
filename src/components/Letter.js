914e1961ac62a8dff745d4c4cc9c9e454ab48453
sanikatawate <sanikatawate30@gmail.com>
2023-09-02 16:39:35 +0530

    Backup

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,29 +1,18 @@
 import React from 'react'
 import "./Main.css"
 
+let style;
 function Letter(props) {
-    // let style=""
-    // if(props.value){
-    //   style = "correct"
-    // }
-    // else{
-    //   style = "wrong"
-    // }
-
-    // if(props.check ===1)
-    //     style = "correct"
-    // else if(props.check === -1)
-    //     style = "wrong"
-    // else
-    //     style = ''
-    // console.log(props.letter)
-
+  let style
+    if(props.check ===1)
+        style = "correct"
+    else if(props.check === -1)
+        style = "wrong"
   return (
     <>
-    {props.letter===" " && <span style={{width:"15px"}}></span>}
-    <div id={props.id}>{props.letter}</div>
+        {props.letter===" " && <span style={{width:"15px"}}></span>}
+        <div className={style}> {props.letter} </div>        
     </>
-    
   )
 }
 
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,80 +1,96 @@
 import React from 'react'
-import "./Main.css"
-import { faker } from "@faker-js/faker";
-import refresh from "../assets/images/refresh.jpeg"
-import Letter from './Letter';
-import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import "./Main.css" 
+import { faker } from "@faker-js/faker"; // To Generate Paragraph
+import refresh from "../assets/images/refresh.jpeg" //Refresh Icon
+import { useState, createContext } from "react"
 import MyTimer from './Timer'
+import Word from './Word';
+import Letter from './Letter'
 
-    const lengthpara = 120
-    const para = faker.word.words(lengthpara).toLowerCase()
+const timeLeft = 30;
+const wordcount = 30;
+const randomwords = faker.random.words(wordcount).toLowerCase();
 
-    let index;
-    var dict = []
-      para.split('').forEach(item => {
-      dict.push(item)
-      index = index+1
-    });
-
-    const time = new Date();
-    time.setSeconds(time.getSeconds()+30); // 10 minutes timer
-  
 function MainContent() {
 
-  const [mainpara , setMainPara] = useState(para)
-  const [mainDict, setMainDict] = useState(dict)
-  const [state, setState] = useState(false)
-  const [typed, setTyped] = useState(0)
+  const [state, setState] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [status, setStatus] = useState('inactive')
+  const [redcount, setRedcount] = useState(0)
+  const [whitecount, setWhitecount] = useState(0)
+  const [wordcount, setWordcount] = useState(0)
 
-  const generate=()=>{
-    setMainPara(faker.word.words(lengthpara).toLowerCase())
-    var dict = []
-    mainpara.split('').forEach(item => {
-    dict.push(item)
-    index = index+1
-    });
-    setMainDict(dict)
-    setState(false)
-    setTyped(0)
-    for(var i=0; i<400; i++)
-    {
-      document.getElementById(`${i}`).style.color ='#848887';
-    }
-  }
+  const time = new Date();
+  time.setSeconds(time.getSeconds()+30); // 10 minutes timer
 
-  const handler = (e)=>{
-    console.log(state)
+  // const generate=()=>{
+  //   setMainPara(faker.word.words(lengthpara).toLowerCase())
+  //   setMainDict(mainpara.split(' '))
+  //   setState(false)
+  // }
+  //   if(i){
+  //     for(var i=0; i<400; i++)
+  //   {
+  //     document.getElementById(`${i}`).style.color ='#848887';
+  //   }
+  //   }
+  // }
+
+  const dict =[]
+  randomwords.split('').forEach(item => {
+    dict.push({letter: item, check:0})
+  });
+  console.log(dict)
+
+  const [words, setWords] = useState(dict)
+
+  const handler = (event) => {
     
-    console.log(e.nativeEvent.key,mainDict[typed])
-    if(e.nativeEvent.key===dict[typed]){
-      document.getElementById(`${typed}`).style.color ='white';
+    if(index===0)
+      setStatus(true)
+    let check;
+    if (words[index].letter === event.nativeEvent.key) 
+    {
+      check = 1;
+      setWhitecount(whitecount+1) 
+
+      if(event.nativeEvent.key===' '){
+        check = 0;
+        setWordcount(wordcount+1)}
     }
-    else{
-      document.getElementById(`${typed}`).style.color ='red';
+    else
+    {
+      check = -1;
+      setRedcount(redcount+1)
     }
-    setTyped(typed+1)
-  }
- 
+    const duplicatearr = [...words]
+    duplicatearr[index].check = check
+    setWords(duplicatearr)
+    setIndex(index + 1);
+  };
+
   return (
     <div className='main-content'>
       <div>
         {state && <MyTimer expiryTimestamp={time}/>}
       </div>
       <div className='para' id='para'>
-
-          {/* {word_arr.map((word, index)=>{
-            return(<div className='word'>{word.split('').map(letter=><Letter letter={letter}/>)}</div>)
+          {/* { mainDict.map((word, index)=>{
+            return(<Word word={word} key={index} letter={word.letter} check={word.check}/>)
           })} */}
-          
-          {mainDict.map((word, index)=>{
-            return(<Letter letter={word} id={index} key={index}/>)
-          })}
+          {words.map((word, index) => {
+          return <Letter key={index} letter={word.letter} check={word.check}></Letter>;
+        })}
       </div>
-
-        <button className='refresh'><img alt='' src={refresh} onClick={generate} width="50px" height="50px"/></button>
-        <input hidden type="text" onKeyPress={(e) => {handler(e); setState(true)}} autoFocus />
+      <button className='refresh'><img alt='' src={refresh} width="50px" height="50px"/></button>
+      <input id='text' type="text" onKeyPress={(e) => {handler(e); setState(true)}} autoFocus onBlur={({ target }) => {target.focus();console.log(target)} } />
     </div>
   )
 }
 
 export default MainContent
+
+ {/* {word_arr.map((word, index)=>{
+            return(<div className='word'>{word.split('').map(letter=><Letter letter={letter}/>)}</div>)
+          })} */}
